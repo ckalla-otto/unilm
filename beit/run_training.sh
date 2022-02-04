bd63f5773aadcf0de80e54d8e501038d9736c08c
Christian Kalla <christian.kalla@otto.de>
2022-02-04 16:01:30 +0100

    removed disabling eval

--- beit/run_training.sh
+++ beit/run_training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python run_class_finetuning.py --model beit_base_patch16_224 --data_path "generated/cache/basic_color_samples" \
-                --nb_classes 43 --data_set "tfrecord" --disable_eval_during_finetuning \
+                --nb_classes 43 --data_set "tfrecord" \
                 --finetune "https://unilm.blob.core.windows.net/beit/beit_base_patch16_224_pt22k_ft22k.pth" \
                 --output_dir "output_dir" --batch_size 64 --lr 2e-3 \
                 --warmup_epochs 5 --epochs 90 --layer_decay 0.75 --drop_path 0.2 \
