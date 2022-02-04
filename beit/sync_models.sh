19be04bc3461767419c8b177bf1e5b475ca7a309
Christian Kalla <christian.kalla@otto.de>
2022-02-04 18:16:07 +0100

    added printout

--- beit/sync_models.sh
+++ beit/sync_models.sh
@@ -3,6 +3,7 @@ OUTPUT_DIR="output_dir"
 DEST_DIR="gs://brain-ai-image-dev-artifacts/dali_usecase-image-similarity/training/beit_orig_finetuning"
 while true  
 do  
+  echo "Syncing..."
   gsutil rsync -r $OUTPUT_DIR $DEST_DIR
   sleep 300  
 done
