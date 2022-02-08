3844bc0aa69eb10c72c7e19ea084472fb879ee16
Christian Kalla <christian.kalla@otto.de>
2022-02-08 17:31:52 +0100

    adapted to hackathon dataset

--- beit/copy_data_from_bucket_sampled.sh
+++ beit/copy_data_from_bucket_sampled.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
-ROOT_DIR=${SCRIPT_DIR}/../../
-
-# shellcheck disable=SC2164
-cd "${ROOT_DIR}"
-
-DATA_FOLDER="generated/cache/basic_color_samples"
-gsutil cp gs://brain-ai-image-dev-artifacts/dali_usecase-basic-color/7bef03ae60b5a2278e702b67a81a04c630b3c348/tensorflow_datasets/dataset_builder_basic_color/0.10.0/dataset_builder_basic_color-train.tfrecord-00006-of-00064 $DATA_FOLDER/dataset_builder_basic_color-train.tfrecord-00006-of-00064
-gsutil cp gs://brain-ai-image-dev-artifacts/dali_usecase-basic-color/7bef03ae60b5a2278e702b67a81a04c630b3c348/tensorflow_datasets/dataset_builder_basic_color/0.10.0/dataset_builder_basic_color-validation.tfrecord-00000-of-00008 $DATA_FOLDER/dataset_builder_basic_color-validation.tfrecord-00000-of-00008
+DATA_FOLDER="generated/cache/hackathon"
+mkdir -p $DATA_FOLDER
+gsutil cp gs://brain-ai-image-dev-artifacts/hackathon_2022-02/tensorflow_datasets/dataset_builder_hackathon/0.2.0/dataset_builder_hackathon-train.tfrecord-00019-of-00256 $DATA_FOLDER/dataset_builder_hackathon-train.tfrecord-00019-of-00256
+gsutil cp gs://brain-ai-image-dev-artifacts/hackathon_2022-02/tensorflow_datasets/dataset_builder_hackathon/0.2.0/dataset_builder_hackathon-test.tfrecord-00000-of-00256 $DATA_FOLDER/dataset_builder_hackathon-test.tfrecord-00000-of-00256
 
